55858dc6b4e0a5b101abc6be4b5ce564c6815918
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2023-09-16 22:47:49 -0300

    :sparkles: refatorando para tornar a classe mais rica.

--- br.com.algaworks.calculadora-imc/src/Paciente.java
+++ br.com.algaworks.calculadora-imc/src/Paciente.java
@@ -1,12 +1,15 @@
 public class Paciente {
-    double altura;
+
     double peso;
+    double altura;
 
     IndiceMassaCorporal calcularIndiceMassaCorporal() {
         IndiceMassaCorporal imc = new IndiceMassaCorporal();
-        imc.altura = this.altura;
-        imc.peso = this.peso;
-        imc.resultado =  this.peso / (this.altura * this.altura);
+        imc.resultado = peso / (altura * altura);
+        imc.peso = peso;
+        imc.altura = altura;
+
         return imc;
     }
+
 }
