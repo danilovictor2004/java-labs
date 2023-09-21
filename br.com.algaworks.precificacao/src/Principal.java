c044d4592382535f1d3df8af389dd3fa0f74fd96
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2023-09-21 17:06:02 -0300

    :sparkles: feat: Atributos de classe com o modificador static.

--- br.com.algaworks.precificacao/src/Principal.java
+++ br.com.algaworks.precificacao/src/Principal.java
@@ -1,14 +1,15 @@
 public class Principal {
     public static void main(String[] args) {
         Produto produto = new Produto();
+        Produto.custoEmbalagem = 20;
 
         produto.alterarPrecoCusto(100);
         ServicoDePrecificacao servicoDePrecificacao = new ServicoDePrecificacao();
         servicoDePrecificacao.definirPrecoVenda(produto, 20);
 
         System.out.printf("Preço de compra: %.2f%n", produto.precoCusto);
+        produto.imprimirCustoEmbalagem();
         System.out.printf("Preço de venda: %.2f%n", produto.precoVenda);
     }
-
 
 }
