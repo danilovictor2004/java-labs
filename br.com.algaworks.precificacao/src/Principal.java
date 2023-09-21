bb6a549ecf3d77b12a38b20b95e43c77cb6db9fc
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2023-09-21 11:23:58 -0300

    :sparkles: feat: Usando a palavra-chave this na classe Produto.

--- br.com.algaworks.precificacao/src/Principal.java
+++ br.com.algaworks.precificacao/src/Principal.java
@@ -1,11 +1,12 @@
 public class Principal {
     public static void main(String[] args) {
         Produto produto = new Produto();
-        produto.precoCusto = 100;
 
+        produto.alterarPrecoCusto(100);
         ServicoDePrecificacao servicoDePrecificacao = new ServicoDePrecificacao();
         servicoDePrecificacao.definirPrecoVenda(produto, 20);
 
+        System.out.printf("Preço de compra: %.2f%n", produto.precoCusto);
         System.out.printf("Preço de venda: %.2f%n", produto.precoVenda);
     }
 
--- br.com.algaworks.precificacao/src/Produto.java
+++ br.com.algaworks.precificacao/src/Produto.java
@@ -3,4 +3,7 @@ public class Produto {
     double precoCusto;
     double precoVenda;
 
+    void alterarPrecoCusto(double precoCusto) {
+        this.precoCusto = precoCusto;
+    }
 }
