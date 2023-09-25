993494dfc905834ad24df07041e57374ee5acff7
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2023-09-25 12:57:29 -0300

    :sparkles: feat: Sobrecarga de métodos.

--- br.com.algaworks.portaria/src/CadastroVisitante.java
+++ br.com.algaworks.portaria/src/CadastroVisitante.java
@@ -1,6 +1,12 @@
 public class CadastroVisitante {
 
-    void cadastrar(Visitante visitante, final int tempoExpiracaoEmMeses) {
+    static final int TEMPO_EXPIRACAO_PADRAO_EM_MESES = 1;
+
+    void cadastrar(final Visitante visitante) {
+        this.cadastrar(visitante, TEMPO_EXPIRACAO_PADRAO_EM_MESES);
+    }
+
+    void cadastrar(final Visitante visitante, final int tempoExpiracaoEmMeses) {
         final int tempoExpiracaoEmDias = tempoExpiracaoEmMeses * 30;
 
         System.out.printf("Visitante %s cadastrado para %d dias%n",
