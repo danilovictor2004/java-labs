70c4d3d9f58610d2bebb672890d2628a9446c37b
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2023-09-25 07:57:49 -0300

    :sparkles: feat: Cria classe FormasGeomÃetricas e métodos estáticos.

---
+++ br.com.algaworks.desafio-5.35/src/FormasGeometricas.java
@@ -0,0 +1,12 @@
+public class FormasGeometricas {
+    public static final double PI = 3.1415;
+
+    public static double calcularAreaQuadrado(double area) {
+        return area * area;
+    }
+
+    public static double calcularAreaCirculo (double raio) {
+        return (raio * raio) * PI;
+    }
+
+}
