0357eb17a1725d307030c73f74c4d571eb0daa7d
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2023-09-26 10:12:35 -0300

    :sparkles: feat: Resolve o desafio 5.41

---
+++ br.com.algaworks.desafio-5.41/src/Principal.java
@@ -0,0 +1,8 @@
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println(NumeroUtil.maiorNumero(10, 9));
+        System.out.println(NumeroUtil.maiorNumero(7, 8, 9));
+        System.out.println(NumeroUtil.maiorNumero(2.9, 1.8));
+        System.out.println(NumeroUtil.maiorNumero(30.0, 30.1, 20.7));
+    }
+}
