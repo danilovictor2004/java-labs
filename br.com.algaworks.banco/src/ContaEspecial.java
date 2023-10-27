9a1c390d63a967ab999949fb3d39bb5ca9fb6c2d
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2023-10-27 10:53:48 -0300

    :sparkles: feat: Aplica o conceito override

--- br.com.algaworks.banco/src/ContaEspecial.java
+++ br.com.algaworks.banco/src/ContaEspecial.java
@@ -23,4 +23,29 @@ public class ContaEspecial extends ContaInvestimento {
         sacar(getTarifaMensal());
     }
 
+    public double getSaldoDisponivel() {
+        return getSaldo() + getLimiteChequeEspecial();
+    }
+
+    public void imprimirDemonstrativo() {
+        System.out.println();
+        System.out.printf("Agência: %d%n", getAgencia());
+        System.out.printf("Conta: %d%n", getNumero());
+        System.out.printf("Titular: %s%n", getTitular().getNome());
+        System.out.printf("Saldo: %.2f%n", getSaldo());
+        System.out.printf("Saldo disponível: %.2f%n", getSaldoDisponivel());
+    }
+
+    public void sacar(double valorSaque) {
+        if (valorSaque <= 0) {
+            throw new IllegalArgumentException("Valor do saque deve ser maior que 0");
+        }
+
+        if (getSaldoDisponivel() < valorSaque) {
+            throw new IllegalArgumentException("Saldo insuficiente para saque");
+        }
+
+        //saldo -= valorSaque;
+    }
+
 }
