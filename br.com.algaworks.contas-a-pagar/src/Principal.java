61b6362622e91a8d3395993311db679c170692c7
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2023-11-09 07:54:19 -0300

    :sparkles: feat: Implementa a primeira interface

--- br.com.algaworks.contas-a-pagar/src/Principal.java
+++ br.com.algaworks.contas-a-pagar/src/Principal.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Principal {
+import model.Holerite;
+import model.OrdemServico;
+import pagamento.Beneficiario;
+import servico.ServicoContaPagar;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        ServicoContaPagar servicoContaPagar = new ServicoContaPagar();
+
+        Beneficiario beneficiario = new Beneficiario("João da Silva", "1234567", "1234");
+        Holerite holerite = new Holerite(beneficiario, 100, 160);
+
+        Beneficiario beneficiario1 = new Beneficiario("Consultoria XYZ", "123455555577", "01456");
+        OrdemServico os = new OrdemServico(beneficiario1, 50000);
+
+        servicoContaPagar.pagar(holerite);
+        servicoContaPagar.pagar(os);
+
+    }
+
 }
