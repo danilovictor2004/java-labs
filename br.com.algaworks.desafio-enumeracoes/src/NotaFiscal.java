27dd4cf0948378d81a9ff5406f3b0d7b9726d668
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2023-12-28 11:11:44 -0300

    :sparkles: feat: Resolução do desafio.

--- br.com.algaworks.desafio-enumeracoes/src/NotaFiscal.java
+++ br.com.algaworks.desafio-enumeracoes/src/NotaFiscal.java
@@ -1,13 +1,9 @@
 public class NotaFiscal {
 
-    public static final int STATUS_NAO_EMITIDA = 0;
-    public static final int STATUS_EMITIDA = 1;
-    public static final int STATUS_CANCELADA = 2;
-
     private final Integer numero;
     private final String descricao;
     private final double valor;
-    private int status = STATUS_NAO_EMITIDA;
+    private StatusNotaFiscal status = StatusNotaFiscal.NAO_EMITIDA;
 
     public NotaFiscal(Integer numero, String descricao, double valor) {
         this.numero = numero;
@@ -27,37 +23,29 @@ public class NotaFiscal {
         return valor;
     }
 
-    public int getStatus() {
+    public StatusNotaFiscal getStatus() {
         return status;
     }
 
     public void cancelar() {
-        if ((status == STATUS_EMITIDA && getValor() >= 1_000)
-                || status == STATUS_CANCELADA) {
-            throw new IllegalStateException("Não foi possível cancelar a nota fiscal");
+        if (getStatus().podeMudarParaCancelado(getValor())) {
+            status = StatusNotaFiscal.CANCELADA;
+        } else {
+            throw new IllegalStateException("Não foi possível cancelar a nota fiscal.");
         }
-
-        status = STATUS_CANCELADA;
     }
 
     public void emitir() {
-        if (status == STATUS_EMITIDA || status == STATUS_CANCELADA) {
-            throw new IllegalStateException("Não foi possível emitir a nota fiscal");
+        if (getStatus().podeMudarParaCancelado(getValor())) {
+            status = StatusNotaFiscal.EMITADA;
+        } else {
+            throw new IllegalStateException("Não foi possível emitir a nota fiscal.");
         }
-
-        status = STATUS_EMITIDA;
     }
 
     public String getDescricaoCompleta() {
-        String descricaoStatus = switch (status) {
-            case STATUS_NAO_EMITIDA -> "Não emitida";
-            case STATUS_EMITIDA -> "Emitida";
-            case STATUS_CANCELADA -> "Cancelada";
-            default -> throw new RuntimeException("Status não tratado");
-        };
-
         return String.format("Nota fiscal #%d (%s) no valor de R$%.2f está %s",
-                getNumero(), getDescricao(), getValor(), descricaoStatus);
+                getNumero(), getDescricao(), getValor(), getStatus().getDescricao());
     }
 
 }
