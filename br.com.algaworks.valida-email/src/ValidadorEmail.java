21c2cdc97a8f070ecf9a5826e160bd26b6b29e93
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2024-01-03 11:51:44 -0300

    :sparkles: feat: reimplementa o metodo validar

--- br.com.algaworks.valida-email/src/ValidadorEmail.java
+++ br.com.algaworks.valida-email/src/ValidadorEmail.java
@@ -1,7 +1,10 @@
 public class ValidadorEmail {
 
+    private static final String EMAIL_REGEX =
+            "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}$";
+
     public static boolean validar(String email) {
-        return false;
+        return email.matches(EMAIL_REGEX);
     }
 
 }
