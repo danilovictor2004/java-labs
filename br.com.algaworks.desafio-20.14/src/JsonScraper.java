393241525a81cf551b657c2d622c0264925ba159
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2024-01-05 11:14:46 -0300

    feat: formata texto em text blocks

--- br.com.algaworks.desafio-20.14/src/JsonScraper.java
+++ br.com.algaworks.desafio-20.14/src/JsonScraper.java
@@ -1,19 +1,22 @@
+import java.util.regex.Pattern;
+
 public class JsonScraper {
 
     public static void main(String[] args) {
-        String json = "{\n" +
-                "    \"id\": 1,\n" +
-                "    \"nome\": \"João da Silva\"\n" +
-                "},\n" +
-                "{\n" +
-                "    \"id\": 2,\n" +
-                "    \"nome\":\"Maria Abadia\"\n" +
-                "},\n" +
-                "{\n" +
-                "    \"id\": 3,\n" +
-                "    \"nome\":\n" +
-                "        \"Sebastião Carvalho\"\n" +
-                "}";
+        String json = """
+                {
+                    "id": 1,
+                    "nome": "João da Silva"
+                },
+                {
+                    "id": 2,
+                    "nome": "Maria Abadia"
+                },
+                {
+                    "id": 3,
+                    "nome": "Sebastião Carvalho"
+                }""";
+
     }
 
 }
