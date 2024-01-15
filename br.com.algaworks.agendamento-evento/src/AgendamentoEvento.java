0a4466eaf344b96404fb737050b73a5888030e0a
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2024-01-15 14:04:29 -0300

    feat: remove os imports no main

--- br.com.algaworks.agendamento-evento/src/AgendamentoEvento.java
+++ br.com.algaworks.agendamento-evento/src/AgendamentoEvento.java
@@ -1,13 +1,3 @@
-import java.time.DateTimeException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.time.format.FormatStyle;
-import java.util.Locale;
-import java.util.Scanner;
-
 public class AgendamentoEvento {
 
     public static void main(String[] args) {
