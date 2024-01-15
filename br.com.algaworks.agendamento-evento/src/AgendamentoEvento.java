c60bbcd3b0be75dbd021bd35d7ddc6c00b349203
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2024-01-15 11:58:59 -0300

    feat: Cria constantes

--- br.com.algaworks.agendamento-evento/src/AgendamentoEvento.java
+++ br.com.algaworks.agendamento-evento/src/AgendamentoEvento.java
@@ -1,8 +1,23 @@
+import java.sql.SQLOutput;
+import java.text.Normalizer;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AgendamentoEvento {
+
+
+    private static final Scanner ENTRADA = new Scanner(System.in);
+    private static final DateTimeFormatter FORMATADOR_DATA = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    private static final DateTimeFormatter FORMATADOR_HORARIO = DateTimeFormatter.ofPattern("HH:mm");
+    private static final DateTimeFormatter FORMATADOR_COMPLETO_BRASIL = DateTimeFormatter
+            .ofLocalizedDateTime(FormatStyle.FULL, FormatStyle.SHORT)
+            .withLocale(new Locale("pt", "BR"));
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
     }
 
 }
