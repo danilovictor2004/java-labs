8b2823a5a3a4845756e26e58279f2e8311a5468f
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2024-01-15 13:25:03 -0300

    feat: cria metodo que requisita hora

--- br.com.algaworks.agendamento-evento/src/AgendamentoEvento.java
+++ br.com.algaworks.agendamento-evento/src/AgendamentoEvento.java
@@ -1,10 +1,12 @@
+import java.time.DateTimeException;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.time.format.FormatStyle;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class AgendamentoEvento {
-
 
     private static final Scanner ENTRADA = new Scanner(System.in);
     private static final DateTimeFormatter FORMATADOR_DATA = DateTimeFormatter.ofPattern("dd/MM/yyyy");
@@ -15,6 +17,19 @@ public class AgendamentoEvento {
 
     public static void main(String[] args) {
 
+    }
+
+    private static LocalDate requisitarData(String descricao) {
+        while (true) {
+            try {
+                System.out.print(descricao);
+                String dataTexto = ENTRADA.nextLine();
+
+                return LocalDate.parse(dataTexto, FORMATADOR_DATA);
+            } catch (DateTimeParseException e) {
+                System.out.println("Data inválida. Tente novamente.");
+            }
+        }
     }
 
 }
