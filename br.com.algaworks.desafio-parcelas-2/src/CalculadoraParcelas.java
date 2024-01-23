66fe340f3e4f2832a18b7dd50ab3a748ebc49d19
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2024-01-23 08:24:34 -0300

    :sparkles: feat: resolução desafio

--- br.com.algaworks.desafio-parcelas-2/src/CalculadoraParcelas.java
+++ br.com.algaworks.desafio-parcelas-2/src/CalculadoraParcelas.java
@@ -1,4 +1,32 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class CalculadoraParcelas {
 
+    private static final DateTimeFormatter FORMATADOR = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    private static final Scanner ENTRADA = new Scanner(System.in);
+
+    private LocalDate dataVencimento() {
+        System.out.print("Data da primeira parcela: ");
+        String data = ENTRADA.nextLine();
+
+        return LocalDate.parse(data, FORMATADOR);
+    }
+
+    private int totalParcelas() {
+        System.out.print("Quantidade de parcelas: ");
+        return ENTRADA.nextInt();
+    }
+
+    public void gerarParcela() {
+        LocalDate date = dataVencimento();
+        int parcela = totalParcelas();
+
+        for (long i = 0; i < parcela; i++) {
+            System.out.printf("Parcela #%d - %s%n", i + 1,
+                    FORMATADOR.format(date.plusMonths(i)));
+        }
+    }
 
 }
