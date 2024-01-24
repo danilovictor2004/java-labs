e9ced156da81cd268ba1870a837a42695bb42fc7
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2024-01-24 08:15:23 -0300

    :tada: feat: first commit

---
+++ br.com.algaworks.idade-paciente/src/IdadePaciente.java
@@ -0,0 +1,13 @@
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class IdadePaciente {
+
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final DateTimeFormatter FORMATADOR_DATA = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public static void main(String[] args) {
+
+    }
+
+}
