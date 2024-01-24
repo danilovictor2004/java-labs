710eda09268f22d01e94d78bf4fda73c60dea8ff
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2024-01-24 08:18:46 -0300

    feat: Método que requisita a data de nascimento do paciente

--- br.com.algaworks.idade-paciente/src/IdadePaciente.java
+++ br.com.algaworks.idade-paciente/src/IdadePaciente.java
@@ -1,13 +1,28 @@
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
 public class IdadePaciente {
 
-    private static final Scanner scanner = new Scanner(System.in);
+    private static final Scanner ENTRADA = new Scanner(System.in);
     private static final DateTimeFormatter FORMATADOR_DATA = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     public static void main(String[] args) {
 
+    }
+
+    private static LocalDate requisitarData(String descricao) {
+        while (true) {
+            try {
+                System.out.print(descricao);
+                String dataTexto = ENTRADA.nextLine();
+
+                return LocalDate.parse(dataTexto, FORMATADOR_DATA);
+            } catch (DateTimeParseException e) {
+                System.out.println("Data inválido. Tente novamente.");
+            }
+        }
     }
 
 }
