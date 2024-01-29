baad7a1affc21d67057fa3dfd7952e35af26fd72
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2024-01-29 13:28:46 -0300

    feat: Cria metodo que recebe a data da festa

---
+++ br.com.algaworks.desafio-23.33/src/ConviteFesta.java
@@ -0,0 +1,29 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ConviteFesta {
+
+    private static final Scanner ENTRADA = new Scanner(System.in);
+    private static final DateTimeFormatter FORMATADOR_DATA = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    private static final DateTimeFormatter FORMATADOR_HORARIO = DateTimeFormatter.ofPattern("HH:mm");
+    private static final DateTimeFormatter FORMATADOR_COMPLETO = DateTimeFormatter
+            .ofPattern("dd 'de' MMMM 'de' yyyy 'às' HH:mm (zzzz)")
+            .withLocale(new Locale("pt", "BR"));
+
+    private static LocalDate requisitarData() {
+        while (true) {
+            try {
+                System.out.print("Local data da festa: ");
+                String dataTexto = ENTRADA.nextLine();
+
+                return LocalDate.parse(dataTexto, FORMATADOR_DATA);
+            } catch (DateTimeParseException e) {
+                System.out.println("Data inválida. Tente novamente");
+            }
+        }
+    }
+
+}
