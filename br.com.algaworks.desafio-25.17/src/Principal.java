3ea1e55fbe92252869ad3d13959acc0f39e9e212
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2024-02-08 11:02:16 -0300

    :sparkles: resolução do desafio

--- br.com.algaworks.desafio-25.17/src/Principal.java
+++ br.com.algaworks.desafio-25.17/src/Principal.java
@@ -1,5 +1,8 @@
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.*;
 
 public class Principal {
 
@@ -11,14 +14,42 @@ public class Principal {
 		funcionarios.add(new Funcionario("Sebastião", new BigDecimal("12000"), false));
 
 		// TODO remover funcionários inativos
+//		Predicate<Funcionario> funcionario = funcionario1 -> funcionario1.isInativo();
+//		funcionarios.removeIf(funcionario);
+
+		Predicate<Funcionario> predicate = Funcionario::isInativo;
+		funcionarios.removeIf(predicate);
 
 		// TODO ordenar funcionários pelo valor do salário
+//		ToDoubleFunction<Funcionario> salarioFunction = funcionario -> funcionario.getSalario().doubleValue();
+//		funcionarios.sort(Comparator.comparingDouble(salarioFunction).reversed());
+
+//		Function<Funcionario, BigDecimal> function = funcionario -> funcionario.getSalario();
+//		funcionarios.sort(Comparator.comparing(function).reversed());
+
+		Function<Funcionario, BigDecimal> function = Funcionario::getSalario;
+		funcionarios.sort(Comparator.comparing(function).reversed());
 
 		// TODO iterar e imprimir funcionários usando o método estático imprimir
+
+//		for (var funcionario : funcionarios) {
+//			imprimir(funcionario);
+//		}
+
+//		Consumer<Funcionario> consumer = funcionario -> imprimir(funcionario);
+//		funcionarios.forEach(consumer);
+
+		Consumer<Funcionario> consumer = Principal::imprimir;
+		funcionarios.forEach(consumer);
+
 	}
 
 	private static void imprimir(Funcionario funcionario) {
 		// TODO implementar a impressão do nome, salário e impostos (20%)
+		System.out.printf("Nome: %s%n", funcionario.getNome());
+		System.out.printf("Salário: %.2f%n", funcionario.getSalario());
+		System.out.printf("Imposto: %.2f%n", funcionario.getSalario().multiply(new BigDecimal("0.20")));
+		System.out.println("----");
 	}
 
 }
