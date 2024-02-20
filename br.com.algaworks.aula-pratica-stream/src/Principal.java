b5be689564f32ba235c1b5d1a360b00dc8486dfa
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2024-02-20 08:50:08 -0300

    feat: Compara o for tradicional com o uso do Stream

--- br.com.algaworks.aula-pratica-stream/src/Principal.java
+++ br.com.algaworks.aula-pratica-stream/src/Principal.java
@@ -1,6 +1,27 @@
+import estoque.CadastroProduto;
+import estoque.Produto;
+
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Principal {
 
     public static void main(String[] args) {
+
+        CadastroProduto cadastroProduto = new CadastroProduto();
+        List<Produto> produtos = cadastroProduto.obterTodos();
+
+//        for (Produto produto : produtos) {
+//            produto.ativar();
+//            System.out.println(produto);
+//        }
+
+        // Usando Stream
+        Stream<Produto> produtoStream = produtos.stream();
+        produtoStream.forEach(produto -> {
+            produto.ativar();
+            System.out.println(produto);
+        });
 
     }
 
