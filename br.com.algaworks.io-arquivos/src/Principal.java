519adca4e20e3ea0fe00dbaa038c9ee181b9d2f7
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2024-03-12 11:17:26 -0300

    feat: Cria subdiretÃorios

--- br.com.algaworks.io-arquivos/src/Principal.java
+++ br.com.algaworks.io-arquivos/src/Principal.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Principal {
+import java.io.File;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        // Criando um diretório em Java
+        File diretorio = new File("C:\\Users\\danillo.caetano\\Documents\\Especialista-Java\\docs");
+        System.out.printf("Diretório criado: %b%n", diretorio.mkdir());
+
+        // Cria diretorio e subdiretorios
+        File diretorio2 = new File("C:\\Users\\danillo.caetano\\Documents\\Especialista-Java\\fotos\\casamento");
+        System.out.printf("Subdiretórios criados: %b%n", diretorio2.mkdirs());
+
+
+    }
+
 }
