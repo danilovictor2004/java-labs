2ff76a2535d25ba36259a6df76f7fd0ef577f043
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2024-03-22 10:54:29 -0300

    :tada: first commit

---
+++ br.com.algaworks.banco-app/src/main/java/com/algaworks/crm/negocio/SaldoInsuficienteException.java
@@ -0,0 +1,9 @@
+package com.algaworks.crm.negocio;
+
+public class SaldoInsuficienteException extends RuntimeException {
+
+    public SaldoInsuficienteException(String message) {
+        super(message);
+    }
+
+}
