03f4e5f399cfa7657f4bf2b8ec93a4b9d8176c90
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2024-03-28 08:22:52 -0300

    feat: Cria um arquivo de configuração properties

--- br.com.algaworks.comercial/src/main/java/repository/RepositoryFactory.java
+++ br.com.algaworks.comercial/src/main/java/repository/RepositoryFactory.java
@@ -1,23 +1,37 @@
 package repository;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class RepositoryFactory implements AutoCloseable{
 
     private final Connection connection;
 
     public RepositoryFactory() {
-        String password = "123456";
-        String url = "jdbc:mysql://localhost:3306/comercial";
-        String user = "root";
 
-        try {
-            this.connection = DriverManager.getConnection(url, user, password);
-        } catch (SQLException e) {
+        try (var is = RepositoryFactory.class.getResourceAsStream("/app.properties")) {
+            Properties properties = new Properties();
+            properties.load(is);
+
+            String url = properties.getProperty("connection.url");
+            String user = properties.getProperty("connection.user");
+            String password = properties.getProperty("connection.password");
+
+            try {
+                this.connection = DriverManager.getConnection(url, user, password);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+
     }
 
     public VendaRepository criarVendaRepositorio() {
