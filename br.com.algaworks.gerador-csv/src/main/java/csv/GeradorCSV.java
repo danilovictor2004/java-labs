c7056db3a0c64cb451da310a11d969b910408fa7
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2024-04-01 10:12:46 -0300

    feat: ler propriedades dinamicamente com Reflection API

--- br.com.algaworks.gerador-csv/src/main/java/csv/GeradorCSV.java
+++ br.com.algaworks.gerador-csv/src/main/java/csv/GeradorCSV.java
@@ -2,12 +2,20 @@ package csv;
 
 import crm.entidade.Cliente;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GeradorCSV {
 
     public static <T> void imprimir(List<Cliente> objetos) {
-        System.out.println("codigo;nome;dataNascimento");
+        Field[] field =  Cliente.class.getDeclaredFields();
+
+        System.out.println(Arrays.stream(field)
+                .map(Field::getName)
+                .collect(Collectors.joining(";")));
+
         objetos.forEach(GeradorCSV::imprimir);
     }
 
