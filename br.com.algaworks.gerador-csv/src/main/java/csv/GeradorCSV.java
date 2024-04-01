0f5c0bcfb5dac058997b6e11aabde26db97d4a55
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2024-04-01 12:38:37 -0300

    feat: Remove os imports não utilizados e refatora a classe Cliente

--- br.com.algaworks.gerador-csv/src/main/java/csv/GeradorCSV.java
+++ br.com.algaworks.gerador-csv/src/main/java/csv/GeradorCSV.java
@@ -6,7 +6,6 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class GeradorCSV {
